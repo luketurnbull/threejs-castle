2c1c6490c2d97103cd41d121af08cb7cb80df811
Luke Turnbull <luke.turn.dev@gmail.com>
2025-06-07 14:39:10 +1000

    Tried implementing grass first using instanced meshes with no shadows from the castle

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { Canvas } from "@react-three/fiber";
 import { Tower } from "./components/tower";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Sky } from "@react-three/drei";
 
 function App() {
   return (
     <div className="h-screen w-screen">
       <Canvas
         camera={{
-          position: [0, 0, 10],
+          position: [0, 50, 150],
           fov: 45,
           near: 0.1,
           far: 1000,
         }}
       >
-        <OrbitControls />
+        <Sky azimuth={1} inclination={0.6} distance={1000} />
+        <OrbitControls
+          minPolarAngle={Math.PI / 2.5}
+          maxPolarAngle={Math.PI / 2.5}
+          enableZoom={false}
+        />
         <ambientLight color="white" intensity={5} />
         <Tower />
       </Canvas>
