7f7fe75b33e4ed1e72bb89d8fb9188e2c47872c6
Luke Turnbull <luke.turn.dev@gmail.com>
2025-06-13 08:29:38 +1000

    Moved AssetLoader to it's own file

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from "react";
-import { useGLTF, useProgress, useTexture } from "@react-three/drei";
 import { gsap } from "gsap";
-import { TEXTURES } from "@/constants/assets";
+import AssetLoader from "./asset-loader";
 
 export default function LoadingScreen({
   onStart,
@@ -57,82 +56,6 @@ export default function LoadingScreen({
             </button>
           )}
         </div>
-      </div>
-    </div>
-  );
-}
-
-function AssetLoader({ onReady }: { onReady: () => void }) {
-  // Preload model
-  useGLTF.preload("/scene.glb", true);
-
-  // Preload tower textures
-  useTexture.preload(TEXTURES.TOWER_DIFFUSE);
-  useTexture.preload(TEXTURES.TOWER_NORMAL);
-  useTexture.preload(TEXTURES.TOWER_ROUGHNESS);
-
-  // Preload hill and grass blade textures
-  useTexture.preload(TEXTURES.BLADE_DIFFUSE);
-  useTexture.preload(TEXTURES.BLADE_ALPHA);
-  useTexture.preload(TEXTURES.HILL_BAKED);
-
-  // Preload rock textures
-  useTexture.preload(TEXTURES.ROCK_DIFFUSE);
-  useTexture.preload(TEXTURES.ROCK_NORMAL);
-
-  const brickRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const { progress: assetsProgress } = useProgress();
-  const [progress, setProgress] = useState(0);
-
-  useEffect(() => {
-    console.log(assetsProgress);
-    setProgress(Math.round(Math.max(assetsProgress - 10, 0)));
-  }, [assetsProgress]);
-
-  useEffect(() => {
-    if (progress === 90) {
-      setTimeout(() => {
-        setProgress(100);
-        onReady();
-      }, 500);
-    }
-
-    const bricksToShow = Math.min(Math.floor(progress / 10), 10);
-
-    brickRefs.current.forEach((brickRef, index) => {
-      if (brickRef) {
-        if (index < bricksToShow) {
-          gsap.to(brickRef, {
-            opacity: 1,
-            scale: 1,
-            duration: 0.5,
-            ease: "back.out(1.7)",
-            delay: index * 0.01,
-          });
-        } else {
-          gsap.set(brickRef, { opacity: 0, scale: 0 });
-        }
-      }
-    });
-  }, [progress, onReady]);
-
-  return (
-    <div className="h-1/3 w-4/5 max-w-[300px] flex flex-col gap-4">
-      <div className="text-center mt-2.5 text-black text-2xl font-bold">
-        {progress}%
-      </div>
-      <div className="grid grid-cols-10 gap-1">
-        {Array.from({ length: 10 }).map((_, index) => (
-          <div
-            key={index}
-            ref={(el) => {
-              brickRefs.current[index] = el;
-            }}
-            className="w-full h-12 opacity-0 transform scale-0"
-          >
-            <img src="/brick-tile.webp" />
-          </div>
-        ))}
       </div>
     </div>
   );
