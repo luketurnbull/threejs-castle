a8af0f3fd9e6fe8c20d9b60cf6c8abd9d0b591c1
Luke Turnbull <luke.turn.dev@gmail.com>
2025-06-19 17:26:28 +1000

    Changed ambient lighting settings
     might be able to remove this once everything is using the night day shader

--- src/components/light-settings.tsx
+++ src/components/light-settings.tsx
@@ -6,13 +6,13 @@ import { NIGHT_TIME_TRANSITION_DURATION } from "@/lib/animation";
 export default function LightSettings() {
   const mode = useAppStore((state) => state.mode);
 
-  const [intensity, setIntensity] = useState(mode === "day" ? 18 : 5);
+  const [intensity, setIntensity] = useState(mode === "day" ? 5 : 1);
 
   useEffect(() => {
     gsap.to(
       { value: intensity },
       {
-        value: mode === "day" ? 18 : 5,
+        value: mode === "day" ? 5 : 1,
         duration: NIGHT_TIME_TRANSITION_DURATION,
         ease: "power2.inOut",
         onUpdate: function () {
