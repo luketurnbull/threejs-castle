d9cbbcaaaf82515865ac55f00c22b626532ff6d3
Luke Turnbull <luke.turn.dev@gmail.com>
2025-06-19 13:28:40 +1000

    Put values in constansts

--- src/components/sky-settings.tsx
+++ src/components/sky-settings.tsx
@@ -2,18 +2,24 @@ import { useAppStore } from "@/store";
 import { Sky, Stars } from "@react-three/drei";
 import { useEffect, useState } from "react";
 import gsap from "gsap";
+import { NIGHT_TIME_TRANSITION_DURATION } from "@/lib/animation";
+
+const SUN_POSITION_DAY = 0.25;
+const SUN_POSITION_NIGHT = -0.75;
 
 export default function SkySettings() {
   const mode = useAppStore((state) => state.mode);
 
-  const [yPos, setYPos] = useState(mode === "day" ? 0.25 : -0.75);
+  const [yPos, setYPos] = useState(
+    mode === "day" ? SUN_POSITION_DAY : SUN_POSITION_NIGHT
+  );
 
   useEffect(() => {
     gsap.to(
       { value: yPos },
       {
-        value: mode === "day" ? 0.25 : -0.75,
-        duration: 1.5,
+        value: mode === "day" ? SUN_POSITION_DAY : SUN_POSITION_NIGHT,
+        duration: NIGHT_TIME_TRANSITION_DURATION,
         ease: "power2.inOut",
         onUpdate: function () {
           setYPos(this.targets()[0].value);
